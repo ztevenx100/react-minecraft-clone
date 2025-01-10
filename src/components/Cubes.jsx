db8acb3a1558e17a7583cf22885ee71011c6ee66
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2025-01-09 23:34:28 -0500

    FIX: Correct syntax and improve ESLint configuration; update dependencies

--- src/components/Cubes.jsx
+++ src/components/Cubes.jsx
@@ -1,10 +1,14 @@
 import { useStore } from '../hooks/useStore'
 import { Cube } from './Cube.jsx'
+import { useMemo } from 'react'
 
 export const Cubes = () => {
-  const [cubes] = useStore(state => [state.cubes])
+  const cubes = useStore(state => state.cubes)
 
-  return cubes.map(({ id, pos, texture }) => {
+  // Memoriza los cubos para evitar renders innecesarios
+  const memoizedCubes = useMemo(() => cubes, [cubes])
+  
+  return memoizedCubes.map(({ id, pos, texture }) => {
     return (
       <Cube
         key={id}
